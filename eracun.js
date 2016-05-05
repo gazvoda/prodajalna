e2b812fea64c18307412e758618fd3cf085ba005
Lenart <gazvoda.lenart@gmail.com>
2016-05-05 21:19:31 +0000

    Dela izpis racuna za stranko

--- eracun.js
+++ eracun.js
@@ -133,7 +133,9 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      callback(vrstice);
+      if (!napaka) {
+        callback(vrstice); 
+      }
     })
 }
 
@@ -142,14 +144,31 @@ var strankaIzRacuna = function(racunId, callback) {
     pb.all("SELECT Customer.* FROM Customer, Invoice \
             WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId,
     function(napaka, vrstice) {
-      callback(vrstice);
+      if (!napaka) {
+        callback(vrstice);
+      }
     })
 }
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
 streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.end();
-})
+    var form = new formidable.IncomingForm();
+  
+    form.parse(zahteva, function (napaka1, polja, datoteke) {
+      if (!napaka1) {
+        pesmiIzRacuna(polja.seznamRacunov, function(pesmi) {
+          strankaIzRacuna(polja.seznamRacunov, function(stranka) {
+            odgovor.setHeader('content-type', 'text/xml');
+            odgovor.render('eslog', {
+              vizualiziraj: true,
+              postavkeRacuna: pesmi,
+              stranka: stranka[0]
+            });
+          });
+        });
+      }
+    });
+});
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
 streznik.get('/izpisiRacun/:oblika', function(zahteva, odgovor) {
@@ -160,6 +179,7 @@ streznik.get('/izpisiRacun/:oblika', function(zahteva, odgovor) {
       odgovor.send("<p>V košarici nimate nobene pesmi, \
         zato računa ni mogoče pripraviti!</p>");
     } else {
+      
       odgovor.setHeader('content-type', 'text/xml');
       odgovor.render('eslog', {
         vizualiziraj: zahteva.params.oblika == 'html' ? true : false,
@@ -178,7 +198,9 @@ streznik.get('/izpisiRacun', function(zahteva, odgovor) {
 var vrniStranke = function(callback) {
   pb.all("SELECT * FROM Customer",
     function(napaka, vrstice) {
-      callback(napaka, vrstice);
+      if (!napaka) {
+        callback(napaka, vrstice);
+      }
     }
   );
 }
